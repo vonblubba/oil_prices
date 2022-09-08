e745be0a6fce5409681a0ab2fae6c83e26404073
vonblubba <oscar.riva@gmail.com>
2022-09-08 11:52:14 +0200

    add OilPriceTag model

---
+++ database/migrations/2022_09_08_085221_create_oil_price_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oil_price_tags', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('date');
+            $table->decimal('price', $precision = 8, $scale = 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oil_price_tags');
+    }
+};
